d1d5d93a7f2a6da1e93d430bf32d36ce6fc77ac6
Anton Vlasenko <vlasenkoanton89@gmail.com>
2021-03-20 10:26:39 +0200

    initial commit

---
+++ src/test/java/com/antonvlasenko/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antonvlasenko.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
