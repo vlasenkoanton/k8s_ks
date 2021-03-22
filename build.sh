b47041e2eee310569c913d75a563fb60472841ad
Anton Vlasenko <vlasenkoanton89@gmail.com>
2021-03-22 10:56:12 +0200

    fix build script

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 
 ./gradlew clean build
 docker build -t antonvlasenko/webapp:0.0.1 -t antonvlasenko/webapp:latest .
-docker push antonvlasenko/webapp:0.0.1 antonvlasenko/webapp:latest
+docker push antonvlasenko/webapp:0.0.1
+docker antonvlasenko/webapp:latest
