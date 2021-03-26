3026bd52fabe22aa03796daa61aea4e9543e5257
Anton Vlasenko <vlasenkoanton89@gmail.com>
2021-03-26 09:50:38 +0200

    StorageController

---
+++ src/main/java/com/antonvlasenko/webapp/StorageController.java
@@ -0,0 +1,27 @@
+package com.antonvlasenko.webapp;
+
+import com.antonvlasenko.webapp.dto.FileDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/storage")
+public class StorageController {
+    private static final String MOUNT_PATH = "/data";
+
+    @PostMapping("write")
+    public void writeFile(@RequestBody FileDto dto) throws IOException {
+        Path path = Paths.get(MOUNT_PATH, dto.getName());
+        Files.writeString(path, dto.getContent());
+    }
+
+    @GetMapping("read")
+    public String readFile(@RequestParam String name) throws IOException {
+        Path path = Paths.get(MOUNT_PATH, name);
+        return Files.readString(path);
+    }
+}
---
+++ src/main/java/com/antonvlasenko/webapp/dto/FileDto.java
@@ -0,0 +1,9 @@
+package com.antonvlasenko.webapp.dto;
+
+import lombok.Data;
+
+@Data
+public class FileDto {
+    private String name;
+    private String content;
+}
