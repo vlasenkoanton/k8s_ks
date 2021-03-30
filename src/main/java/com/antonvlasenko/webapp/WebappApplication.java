c4578f0149a238ebb8031b2636086813bb072821
Anton Vlasenko <vlasenkoanton89@gmail.com>
2021-03-30 09:07:14 +0300

    move app to root

--- src/main/java/com/antonvlasenko/webapp/WebappApplication.java
+++ src/main/java/com/antonvlasenko/webapp/WebappApplication.java
@@ -1,15 +1,15 @@
-package com.antonvlasenko.webapp.web;
+package com.antonvlasenko.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.antonvlasenko.webapp.jpa")
+@EnableJpaRepositories("com.antonvlasenko.webapp.jpa")
 public class WebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebappApplication.class, args);
+    }
 
 }
