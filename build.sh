b33a208f37d3e81047d49ee4bdd6c10bbea2b84a
Anton Vlasenko <vlasenkoanton89@gmail.com>
2021-04-08 09:55:18 +0300

    remove latest

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 ./gradlew clean build
-docker build -t antonvlasenko/webapp:0.0.1 -t antonvlasenko/webapp:latest .
+docker build -t antonvlasenko/webapp:0.0.1 .
 docker push antonvlasenko/webapp:0.0.1
-docker push antonvlasenko/webapp:latest
